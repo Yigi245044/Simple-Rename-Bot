c87e0320c78dffe4085b14d5196e58e74635625d
Yigi245044 <148187799+Yigi245044@users.noreply.github.com>
2024-03-03 08:27:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "21037450"))
+API_HASH = environ.get("API_HASH", "05ac9eb7c523b83c51d89d1f2f91d58b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6686737285:AAFS95C2rwkYtwC-dipW2ePCSTJTTiXxG5Y")
+ADMIN = int(environ.get("ADMIN", "1098077551"))          
+CAPTION = environ.get("CAPTION", "True")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
